32d5329d02dab2f8b2357c97e28188dd532c42d6
uzair-ahmed-01 <ansariuzair4151@gmail.com>
2023-11-26 03:17:53 +0530

    url server

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "https://nodejs-todoapp.onrender.com/api/v1";
+export const server = "https://todp-app-nodejs.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
